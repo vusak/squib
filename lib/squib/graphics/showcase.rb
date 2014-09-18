27139509bbdb2c45deb49bcd04583aa286f86a62
Andy Meneely <andy@se.rit.edu>
2014-09-18 14:36:49 -0400

    Factoring out more parameters

--- lib/squib/graphics/showcase.rb
+++ lib/squib/graphics/showcase.rb
@@ -33,34 +33,45 @@ module Squib
     # @api private
     def render_showcase(range, trim, trim_radius, dir, file_to_save)
 
-      angle = 0.15
+      angle = 0.12
       scale = 0.85
+      offset = 0.95
+      fill_color = :white
 
       out_width = range.size * (@width + 50)
       out_height = @height * (2 + scale + angle)
       out_cc = Cairo::Context.new(Cairo::ImageSurface.new(out_width, out_height))
 
-      out_cc.set_source_color(:black)
+      out_cc.set_source_color(fill_color)
       out_cc.paint
 
       cards = range.collect { |i| @cards[i] }
 
       cards.each_with_index do |card, i|
-        surface = perspective_reflect(card.cairo_surface, angle, scale)
-        out_cc.set_source(surface, 25 + i * (surface.width + 25) ,75)
+        surface = perspective_reflect(trim(card.cairo_surface), angle, scale)
+        out_cc.set_source(surface, 25 + i * (surface.width*offset + 25) ,75)
         out_cc.paint
       end
 
       out_cc.target.write_to_png("#{dir}/#{file_to_save}.png")
     end
 
+    def trim(surface)
+      margin = 39
+      radius = 32
+      trim_cc = Cairo::Context.new(Cairo::ImageSurface.new(surface.width-2.0*margin, surface.height-2.0*margin))
+      trim_cc.rounded_rectangle(0, 0, trim_cc.target.width, trim_cc.target.height, radius, radius)
+      trim_cc.set_source(surface, -1 * margin, -1 * margin)
+      trim_cc.clip
+      trim_cc.paint
+      return trim_cc.target
+    end
+
     def perspective_reflect(surface, angle, scale)
-      tmp_cc = Cairo::Context.new(Cairo::ImageSurface.new(surface.width * (scale+angle/2), surface.height * (2 + scale)))
-      tmp_cc.set_source_color(:gray)
-      tmp_cc.paint
+      tmp_cc = Cairo::Context.new(Cairo::ImageSurface.new(surface.width * (scale+angle/2), surface.height * (2 + angle/2.0)))
 
       #### TOP PART ####
-      scale_x     = scale       # scale the width
+      scale_x     = scale      # scale the width
       scale_y     = 1.0        # same height, not reversed
       rotate_x    = 0.0        # 
       rotate_y    = -1 * angle # don't rotate along y
@@ -71,15 +82,17 @@ module Squib
                                  translate_x, translate_y)
       tmp_cc.transform(matrix)
       tmp_cc.set_source(surface,0,0)
+      
       tmp_cc.paint
 
       tmp_cc.identity_matrix #reset
+      tmp_cc.new_path
 
       #### BOTTOM PART ####
 
       # Transformation
       scale_x     = scale       # scale the width
-      scale_y     = -1.0        # same height, but reversed
+      scale_y     = -1.0  # same height, but reversed
       rotate_x    = 0.0         # 
       rotate_y    = -1 * angle  # don't rotate along y
       translate_x = 0.0         # don't translate
@@ -91,18 +104,17 @@ module Squib
       tmp_cc.set_source(surface,0,0)
 
       # Gradient mask
-      gradient = Cairo::LinearPattern.new(0.0, 1000, 0.0, 1000)
-      gradient.add_color_stop_rgba(0, 0, 0, 0, 0.0)
-      gradient.add_color_stop_rgba(1.0, 0, 0, 0, 0.5)
+      gradient = Cairo::LinearPattern.new(0.0, 0.0, 0.0, 2000)
+      gradient.add_color_stop_rgba(0,   0,0,0, 0.0)
+      gradient.add_color_stop_rgba(1.0, 0,0,0, 0.3)
+      # tmp_cc.set_source(gradient)
+      # tmp_cc.paint
+      # tmp_cc.clip
       tmp_cc.mask(gradient)
 
       # tmp_cc.paint
 
-      
-
-      tmp_cc.target
-
-
+      return tmp_cc.target
     end
 
   end  
--- samples/showcase.rb
+++ samples/showcase.rb
@@ -1,10 +1,13 @@
 require 'squib'
 
 Squib::Deck.new(cards: 4) do
-  background color: '#DED4B9'
+  background color: '#CE534D'
+  rect fill_color: '#DED4B9', x: 78, y: 78, 
+       width: 825-2*78, height: 1125-2*78, radius: 32
   title = %w(Grifter Thief Thug Kingpin)
   text str: title, font: 'Helvetica,Sans weight=800 120', 
        x: 78, y: 78, width: 825-2*78, align: :center
+  svg file: 'spanner.svg', x: (825-500)/2, y: 500, width: 500, height: 500
 
   showcase file: 'showcase_output.png', trim: 78, trim_radius: 32
   
